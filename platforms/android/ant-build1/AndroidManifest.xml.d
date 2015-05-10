b3e42327369d5d4c87771aafea181c70b9ee7367
Paul Harvey <harv0116@algonquinlive.com>
2015-05-10 11:59:40 -0400

    unfix

--- platforms/android/ant-build1/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\wamp\www\shop\platforms\android\ant-build\AndroidManifest.xml : \
-C:\wamp\www\shop\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-gen1/ca/edumedia/harv0116/shop/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ca.edumedia.harv0116.shop;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
